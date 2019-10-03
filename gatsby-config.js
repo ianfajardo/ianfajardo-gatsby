90f4bef584584c54b422dc84ff8949c3c2938e99
Ian Fajardo <ifajardo@arccorp.com>
2019-10-03 14:15:58 -0400

    Updating init styles for personal site

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+ require("dotenv").config({
+   path: `.env.${process.env.NODE_ENV}`,
+ })
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Pandas Eating Lots'
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
+  ]
 }
